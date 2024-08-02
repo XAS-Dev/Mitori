8f99112323394bd9689ef5533d268efb5472961b
XiYang6666 <1782356858@qq.com>
2024-08-02 13:09:40 +0800

    :sparkles: optimize avatar generate

--- Mitori-Velocity/src/main/kotlin/xyz/xasmc/mitori/mitorivelocity/util/PlayerUtil.kt
+++ Mitori-Velocity/src/main/kotlin/xyz/xasmc/mitori/mitorivelocity/util/PlayerUtil.kt
@@ -3,6 +3,9 @@ package xyz.xasmc.mitori.mitorivelocity.util
 import com.velocitypowered.api.proxy.Player
 import xyz.xasmc.mitori.satori.datatype.guildMember.GuildMember
 import xyz.xasmc.mitori.satori.datatype.user.User
+import java.awt.image.BufferedImage
+import java.io.ByteArrayOutputStream
+import javax.imageio.ImageIO
 
 object PlayerUtil {
     fun createSatoriMember(player: Player, joinedAt: Long? = null, baseLink: String? = null): GuildMember {
@@ -13,11 +16,21 @@ object PlayerUtil {
     fun createSatoriUser(player: Player, baseLink: String? = null): User {
         val avatar = if (baseLink != null) "$baseLink/resource/player/avatar/${player.uniqueId}" else null
         return User(
-            id = player.uniqueId.toString(),
-            name = player.username,
-            nick = player.username,
-            avatar = avatar,
-            false
+            id = player.uniqueId.toString(), name = player.username, nick = player.username, avatar = avatar, false
         )
+    }
+
+    fun generateAvatar(player: Player): ByteArray {
+        val skin = ImageIO.read(SkinUtil.getSkin(player).inputStream())
+        val headInner = skin.getSubimage(8, 8, 8, 8)
+        val headOuter = skin.getSubimage(40, 8, 8, 8)
+        val painter = BufferedImage(256, 256, BufferedImage.TYPE_INT_ARGB)
+        val g2d = painter.createGraphics()
+        g2d.drawImage(headInner, 256 / 18, 256 / 18, 256 - 256 / 9, 256 - 256 / 9, null)
+        g2d.drawImage(headOuter, 0, 0, 256, 256, null)
+        g2d.dispose()
+        val output = ByteArrayOutputStream()
+        ImageIO.write(painter, "png", output)
+        return output.toByteArray()
     }
 }
